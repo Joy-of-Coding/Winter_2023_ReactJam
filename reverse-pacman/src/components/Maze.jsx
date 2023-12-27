a61b0393c9f0d86d1be3835cacc7b16ac68027f1
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-12-27 17:44:42 -0500

    Static Maze Added to board

--- reverse-pacman/src/components/Maze.jsx
+++ reverse-pacman/src/components/Maze.jsx
@@ -40,21 +40,53 @@ function Maze() {
     ];
 
 
+    function drawWall(ctx, column, row) {
+        ctx.fillStyle = "rgba(0, 0, 255, 0.8)";
+        ctx.fillRect(column * tileSize, row * tileSize, tileSize, tileSize);
+    }
+
+    function drawDot (ctx, column, row) {
+        ctx.fillStyle = "yellow";  // yellow color for pellets
+        ctx.beginPath();
+        // Calculate the center of the tile for the arc
+        let centerX = column * tileSize + tileSize / 2;
+        let centerY = row * tileSize + tileSize / 2;
+        ctx.arc(centerX, centerY, tileSize / 6, 0, 2 * Math.PI); // smaller radius for pellet
+        ctx.fill();
+    }
+
+
+    function draw(ctx) {
+            for (let row= 0; row < map.length; row ++) {
+                for (let column = 0; column < map[row].length; column++){
+                    let tile = map[row][column];
+                    switch(tile) {
+                        case 1: // Wall
+                            drawWall(ctx, column, row);
+                            break;
+                        case 0: // Pellet
+                            drawDot(ctx, column, row);
+                            break;
+                    }
+                }
+            }
+    }
+
     useEffect(() => {
         const canvas = canvasRef.current;
         canvas.width = map[0].length * tileSize;
         canvas.height =map.length * tileSize;
 
-
         const ctx = canvas.getContext('2d');
 
 
-        // Set the background color
-        ctx.fillStyle = "gray"; // Sets the fill color to gray
+        ctx.fillStyle = "black";
         ctx.fillRect(0, 0, canvas.width, canvas.height); // Draws a rectangle covering the entire canvas
 
         contextRef.current = ctx;
-    }, [map]);
+
+        draw (ctx)
+    }, []);
 
 
     return (
