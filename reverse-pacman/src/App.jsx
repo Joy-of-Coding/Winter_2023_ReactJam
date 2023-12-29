6ad74117d5138a935aa1d748f36db874104d4824
Suzanne Atkinson <suzanneatkinson@gmail.com>
2023-12-29 09:43:07 -0500

    added text to suggest feature additions

--- reverse-pacman/src/App.jsx
+++ reverse-pacman/src/App.jsx
@@ -8,7 +8,13 @@ function App() {
 
   return (
       <>
-        <PlayerMovement />
+          <h2>Add Features</h2>
+          <h3>Collision Detection</h3>
+          <p>Add collision detection to keep the character pixel (red square) inside a chosen boundary areay</p>
+          <h3>Co-op Control</h3>
+          <p>Add arrow key movements to allow "couch" co-op control from one keyboard</p>
+
+          <PlayerMovement />
         <GameArea />
       </>
 
