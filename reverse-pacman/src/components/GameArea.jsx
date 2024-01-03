311d7b8bfefe0730068c692384571ebde41aa61d
John Serwatka <johnchrisserwatka@gmail.com>
2024-01-02 19:28:19 -0500

    testing bar in GameArea, added GameOver.jsx/.css

--- reverse-pacman/src/components/GameArea.jsx
+++ reverse-pacman/src/components/GameArea.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
 import './GameArea.css'
+import GameOver from "./GameOver.jsx";
 function GameArea() {
     const score= 0
     const highScore = 300
+    let isGameOver = false
     return (
         <div className='gameArea'>
+            {/* Testing div, comment out when going for release, leave in code for easy testing */}
+            <div className= 'testingStuff'>
+                <button
+                    onClick={event =>
+                    {GameOver(1, 300);
+                    isGameOver = true}}
+                >
+                    Test Game Over
+                </button>
+            </div>
             <div className='score'>
                 <div>
                     <p>TOP</p>
                     <p>{score}</p>
                 </div>
-                <div>
+                    <div>
                     <p>TOP</p>
                     <p>{highScore}</p>
                 </div>
             </div>
-            <div className='board'>Game Play Area</div>
+            <div className='board'>Game Play Area
+                <div className='gameOver'>
+
+                </div>
+            </div>
         </div>
     )
 }
---
+++ reverse-pacman/src/components/GameOver.jsx
@@ -0,0 +1,10 @@
+{/*
+* Game over called by GameArea
+* Overlay over gameBoard
+* show player score?
+* Controls to play again/ return to main menu
+* */}
+function GameOver(score, highScore){
+<div className='gameOver'>Your Score : {score}   High Score: {highScore}</div>
+}
+export default GameOver
