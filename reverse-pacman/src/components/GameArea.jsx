0b1a6a7860fd1d1a3e31c0cbdc97149ad7d2aa53
Tishtanya Shaw <tish@tishtanyas-air.mynetworksettings.com>
2024-01-02 22:28:25 -0500

    Added 2d array to game area

--- reverse-pacman/src/components/GameArea.jsx
+++ reverse-pacman/src/components/GameArea.jsx
@@ -1,30 +1,62 @@
-import './GameArea.css'
-import PlayerMovement from "./PlayerMovement.jsx";
+import './GameArea.css';
+import PlayerMovement from './PlayerMovement.jsx';
+import { usState, useEffect } from 'react';
 
 function GameArea() {
-    const score= 0
-    const highScore = 300
+	const score = 0;
+	const highScore = 300;
 
-    return (
-        <div className='gameArea'>
-            <div className='score'>
-                <div>
-                    <p>TOP</p>
-                    <p>{score}</p>
-                </div>
-                <div>
-                    <p>TOP</p>
-                    <p>{highScore}</p>
-                </div>
-            </div>
-            <div className='board'>
-                <h1>Game Play Area</h1>
-                <PlayerMovement />
+	const boardDimensions = {
+		width: 10,
+		height: 10,
+	};
 
-            </div>
+	const board = Array(boardDimensions.width)
+		.fill()
+		.map((_, idx) =>
+			Array(boardDimensions.height)
+				.fill()
+				.map((_, index) => index)
+		);
 
-        </div>
-    )
+	return (
+		<div className='gameArea'>
+			<div className='score'>
+				<div>
+					<p>TOP</p>
+					<p>{score}</p>
+				</div>
+				<div>
+					<p>TOP</p>
+					<p>{highScore}</p>
+				</div>
+			</div>
+			<div
+				className='board'
+				style={{
+					display: 'grid',
+					gridTemplateColumns: `repeat(${boardDimensions.width}, 1fr)`,
+					gridTemplateRows: `repeat(${boardDimensions.height}, 1fr)`,
+				}}
+			>
+				{board.map((row, i) =>
+					row.map((col, j) => (
+						<div
+							key={`${i} - ${j}`}
+							style={{
+								border: '1px solid white',
+								display: 'flex',
+								placeContent: 'center center',
+								alignContent: 'center',
+								alignItems: 'center',
+							}}
+						>{`${i} - ${j}`}</div>
+					))
+				)}
+				<PlayerMovement />
+			</div>
+		</div>
+	);
 }
 
-export default GameArea
+export default GameArea;
