290c8a31e8d5df90e9f1074015b065176ddb5f06
John Serwatka <johnchrisserwatka@gmail.com>
2024-01-05 12:28:41 -0500

    added GameOver.jsx/.css

--- reverse-pacman/src/components/GameArea.jsx
+++ reverse-pacman/src/components/GameArea.jsx
@@ -4,19 +4,10 @@ import GameOver from "./GameOver.jsx";
 function GameArea() {
     const score= 0
     const highScore = 300
-    let isGameOver = false
+    /* Change to state based */
+    let isGameOver = true
     return (
         <div className='gameArea'>
-            {/* Testing div, comment out when going for release, leave in code for easy testing */}
-            <div className= 'testingStuff'>
-                <button
-                    onClick={event =>
-                    {GameOver(1, 300);
-                    isGameOver = true}}
-                >
-                    Test Game Over
-                </button>
-            </div>
             <div className='score'>
                 <div>
                     <p>TOP</p>
@@ -28,9 +19,9 @@ function GameArea() {
                 </div>
             </div>
             <div className='board'>Game Play Area
-                <div className='gameOver'>
-
-                </div>
+                { isGameOver ?
+                    (<GameOver score={score} highScore={highScore}/>): null
+                }
             </div>
         </div>
     )
--- reverse-pacman/src/components/GameOver.jsx
+++ reverse-pacman/src/components/GameOver.jsx
@@ -5,6 +5,8 @@
 * Controls to play again/ return to main menu
 * */}
 function GameOver(score, highScore){
-<div className='gameOver'>Your Score : {score}   High Score: {highScore}</div>
+    <div className='gameOver'>
+        Your Score : {score}   High Score: {highScore}
+     </div>
 }
 export default GameOver
